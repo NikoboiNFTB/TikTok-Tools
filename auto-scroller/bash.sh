33a4c4ee3c783fe06b490141cbbef74b9cbeec60
NikoboiNFTB <github@nikoboi.dev>
2026-01-09 22:25:18 +0200

    auto-commit

--- auto-scroller/bash.sh
+++ auto-scroller/bash.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
 # Remove maximization
 for id in 0x02a02f32 0x02a02feb 0x02a0300b 0x02a0302b 0x02a0304b 0x02a0306b 0x02a0308b 0x02a030ab; do
     wmctrl -i -r $id -b remove,maximized_vert,maximized_horz
